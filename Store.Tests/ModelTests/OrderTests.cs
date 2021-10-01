2b960ab312a79fc6bd0e8f127f779c9b5940a0a0
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-10-01 12:47:01 -0700

    order description passes successfully, lunch time

--- Store.Tests/ModelTests/OrderTests.cs
+++ Store.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,27 @@ namespace Store.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-    Order newOrder = new Order();
+    Order newOrder = new Order("test", "placeholder2");
     Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void CreateName_CreatesOrderWithName_String()
+    {
+    string name ="12 pies";
+    string description = "13 apple pies";
+    Order newOrder = new Order(name, description);
+    string result = newOrder.OrderName;
+    Assert.AreEqual(name, result);
+    }
+    [TestMethod]
+    public void CreateName_CreatesOrderWithDescription_String()
+    {
+    string name ="12 pies";
+    string description = "apple";
+    Order newOrder = new Order(name, description);
+    string result = newOrder.OrderDescription;
+    Assert.AreEqual(description, result);
     }
   }
 }
