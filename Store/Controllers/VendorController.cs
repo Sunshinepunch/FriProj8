29a28667638036e4956d5a3572e755ee3520cab5
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-10-01 17:23:23 -0700

    inline styling added to splash page

---
+++ Store/Controllers/VendorController.cs
@@ -0,0 +1,24 @@
+// using System.Collections.Generic;
+// using System;
+// using Microsoft.AspNetCore.Mvc;
+// using Store;
+
+// namespace Store.Controllers
+// {
+//   public class VendorsController : Controller
+//   {
+
+//     [HttpGet("/vendors")]
+//     public ActionResult Index()
+//     {
+//       // List<Vendor> allVendors = Vendor.GetAll();
+//       return View();
+//     }
+
+//     [HttpGet("/vendors/new")]
+//     public ActionResult New()
+//     {
+//       return View();
+//     }
+//   }
+// }
